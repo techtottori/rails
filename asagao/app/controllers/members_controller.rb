eebb0de6c78acab0e303558de25484abcb43b76b
MoriKazunori <samasoni1993@gmail.com>
2017-10-14 14:11:18 +0900

    今日も草を生やしました。

---
+++ asagao/app/controllers/members_controller.rb
@@ -0,0 +1,34 @@
+class MembersController < ApplicationController
+  # 会員一覧
+  def index
+    @members = Member.order("number")
+  end
+
+  # 検索
+  def search
+    @members = Member.search(params[:q])
+    render "index"
+  end
+
+  # 会員情報の詳細
+  def show
+    @member = Member.find(params[:id])
+  end
+
+  def new
+  	@member = Member.new(birthday: Date.new(1980, 1, 1))
+  end
+
+  def edit
+  	@member = Member.find(params[:id])
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- asagao/app/models/member.rb
+++ asagao/app/models/member.rb
@@ -1,2 +1,12 @@
 class Member < ActiveRecord::Base
+  class << self
+    def search(query)
+      rel = order(query)
+      if query.present?
+        rel = rel.where("name LIKE ? OR full_name LIKE ?",
+        	"%#{query}%", "%#{query}%")
+      end
+      rel
+    end
+  end
 end
